c2433c1342ea438e3480bab85d42b10a25688a47
khloe <slowykhloe@gmail.com>
2023-04-11 10:09:02 -0700

    initial commit

---
+++ madLibs.js
@@ -0,0 +1,44 @@
+const readLine=require('readline-sync');
+
+let words = [];
+words[0] = readLine.question("Enter adjective: ");
+words[1] = readLine.question("Enter part of the body: ");
+words[2] = readLine.question("Enter plural noun: ");
+words[3] = readLine.question("Enter place: ");
+words[4] = readLine.question("Enter place: ");
+words[5] = readLine.question("Enter verb ending in ing: ");
+words[6] = readLine.question("Enter noun: ");
+words[7] = readLine.question("Enter plural noun: ");
+words[8] = readLine.question("Enter plural noun: ");
+words[9] = readLine.question("Enter adjective: ");
+words[10] = readLine.question("Enter noun: ");
+words[11] = readLine.question("Enter noun: ");
+words[12] = readLine.question("Enter part of the body: ");
+words[13] = readLine.question("Enter adjective: ");
+words[14] = readLine.question("Enter adjective: ");
+words[15] = readLine.question("Enter noun: ");
+words[16] = readLine.question("Enter plural noun: ");
+words[17] = readLine.question("Enter adjective: ");
+words[18] = readLine.question("Enter noun: ");
+words[19] = readLine.question("Enter adjective: ");
+words[20] = readLine.question("Enter plural noun: ");
+words[21] = readLine.question("Enter plural noun: ");
+words[22] = readLine.question("Enter adjective: ");
+words[23] = readLine.question("Enter noun: ");
+console.log(`Hello Kitty is a/an ${words[0]} girl who wears a 
+red bow on her left ${words[1]}. She loves to travel to 
+fun ${words[2]} like (the) ${words[3]} and (the) ${words[4]}. When she's 
+not ${words[5]} around the world, she likes to relax 
+and read a good ${words[6]} or listen to her favorite ${words[7]}. 
+But her favorite thing to do is make friends with nice ${words[8]} like you! 
+Hello Kitty loves her ${words[9]} family. 
+ Her twin, Mimmy, is also her best ${words[10]} in the whole wide 
+ ${words[11]}. She looks just like Hello Kitty, except she wears a yellow 
+ bow on her right ${words[12]}. Hello Kitty's ${words[13]} parents 
+ are her mama, Mary, and her papa, George. Mama is a kind and 
+ ${words[14]} ${words[15]}. She loves to cook and take care of ${words[16]}. 
+ Papa is always making ${words[17]} jokes. He is a hard working ${words[18]} 
+ and a/an ${words[19]} papa to Hello Kitty and Mimmy. Hello Kitty's 
+ grandma and grandpa are named Margaret and Anthony. Grandpa loves 
+ to paint ${words[20]}. Grandma enjoys making ${words[21]}. Hello 
+ Kitty's family is a very ${words[22]} part of her ${words[23]}!`);
