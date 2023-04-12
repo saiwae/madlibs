0b79e972c285f668903f8b4dcc0a3ef7155b72bd
khloe <slowykhloe@gmail.com>
2023-04-12 09:50:11 -0700

    initial commit extension 3 04/12

---
+++ madLibsExtension3.js
@@ -0,0 +1,44 @@
+const readLine=require('readline-sync');
+
+let words = [];
+words[0] = readLine.question("Enter adjective: ");
+words[1] = readLine.question("Enter part of the body: ");
+words[2] = readLine.question("Enter plural noun: ");
+words[3] = readLine.question("Enter place: ");
+words[4] = readLine.question("Enter place: ");
+words[5] = readLine.question("Enter verb ending in ing: ");
+words[6] = readLine.question("Enter noun: ");
+words[7] = readLine.question("Enter plural noun: ");
+words[8] = readLine.question("Enter plural noun: ");
+words[9] = readLine.question("Enter adjective: ");
+words[10] = readLine.question("Enter noun: ");
+words[11] = readLine.question("Enter noun: ");
+words[12] = readLine.question("Enter part of the body: ");
+words[13] = readLine.question("Enter adjective: ");
+words[14] = readLine.question("Enter adjective: ");
+words[15] = readLine.question("Enter noun: ");
+words[16] = readLine.question("Enter plural noun: ");
+words[17] = readLine.question("Enter adjective: ");
+words[18] = readLine.question("Enter noun: ");
+words[19] = readLine.question("Enter adjective: ");
+words[20] = readLine.question("Enter plural noun: ");
+words[21] = readLine.question("Enter plural noun: ");
+words[22] = readLine.question("Enter adjective: ");
+words[23] = readLine.question("Enter noun: ");
+console.log(`Hello Kitty is a/an ${words[0].toUpperCase()} girl who wears a 
+red bow on her left ${words[1].toUpperCase()}. She loves to travel to 
+fun ${words[2].toUpperCase()} like (the) ${words[3].toUpperCase()} and (the) ${words[4].toUpperCase()}. When she's 
+not ${words[5].toUpperCase()} around the world, she likes to relax 
+and read a good ${words[6].toUpperCase()} or listen to her favorite ${words[7].toUpperCase()}. 
+But her favorite thing to do is make friends with nice ${words[8].toUpperCase()} like you! 
+Hello Kitty loves her ${words[9].toUpperCase()} family. 
+ Her twin, Mimmy, is also her best ${words[10].toUpperCase()} in the whole wide 
+ ${words[11].toUpperCase()}. She looks just like Hello Kitty, except she wears a yellow 
+ bow on her right ${words[12].toUpperCase()}. Hello Kitty's ${words[13].toUpperCase()} parents 
+ are her mama, Mary, and her papa, George. Mama is a kind and 
+ ${words[14].toUpperCase()} ${words[15].toUpperCase()}. She loves to cook and take care of ${words[16].toUpperCase()}. 
+ Papa is always making ${words[17].toUpperCase()} jokes. He is a hard working ${words[18].toUpperCase()} 
+ and a/an ${words[19].toUpperCase()} papa to Hello Kitty and Mimmy. Hello Kitty's 
+ grandma and grandpa are named Margaret and Anthony. Grandpa loves 
+ to paint ${words[20].toUpperCase()}. Grandma enjoys making ${words[21].toUpperCase()}. Hello 
+ Kitty's family is a very ${words[22].toUpperCase()} part of her ${words[23].toUpperCase()}!`);
